2dd2e6694d91be2330888773fba5df51f1f3b4fd
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-08-16 10:15:06 -0700

    page template to stateless component

--- gatsby/src/templates/project.js
+++ gatsby/src/templates/project.js
@@ -3,35 +3,33 @@ import Helmet from "react-helmet";
 import styles from './project.module.css';
 import Media from "react-media";
 
-export default class ProjectTemplate extends Component {
-  render(){
-    const { project } = this.props.data;
-    return (
-  		<div className={styles.projectSingleContainer}>
-        <Helmet title={`Marc Aaron Glasser - ${project.title}`} />
-  			<div className={styles.projectName}>
-  				{project.name}
-  			</div>
-        <img className={`${styles.projectImage} ${styles.projectImageMobile}`} alt={`A screenshot of the ${project.name} application.`} src={project.img.url}/>
-        <img className={`${styles.projectImage} ${styles.projectImageDesktop}`} alt={`A screenshot of the ${project.name} application.`} src={project.imgMock.url}/>
-  			<div className={styles.projectInformation}>
-  				<div className={styles.projectTitle}>
-  					<em>{project.title}</em>
-  				</div>
-  				<div className={styles.projectDescription}>
-  					{project.description}
-  				</div>
-  				<div className={styles.projectTools}>
-  					<em>{project.tools}</em>
-  				</div>
-  				<i className="fas fa-code-branch fa-3x"></i>
-  				<div className={styles.projectUrl}>
-  					<a href={`http://${project.url}`}>{project.url}</a>
-  				</div>
-  			</div>
-  		</div>
-    );
-  }
+const ProjectTemplate = ({data}) => {
+  const { project } = data;
+  return (
+		<div className={styles.projectSingleContainer}>
+      <Helmet title={`Marc Aaron Glasser - ${project.title}`} />
+			<div className={styles.projectName}>
+				{project.name}
+			</div>
+      <img className={`${styles.projectImage} ${styles.projectImageMobile}`} alt={`A screenshot of the ${project.name} application.`} src={project.img.url}/>
+      <img className={`${styles.projectImage} ${styles.projectImageDesktop}`} alt={`A screenshot of the ${project.name} application.`} src={project.imgMock.url}/>
+			<div className={styles.projectInformation}>
+				<div className={styles.projectTitle}>
+					<em>{project.title}</em>
+				</div>
+				<div className={styles.projectDescription}>
+					{project.description}
+				</div>
+				<div className={styles.projectTools}>
+					<em>{project.tools}</em>
+				</div>
+				<i className="fas fa-code-branch fa-3x"></i>
+				<div className={styles.projectUrl}>
+					<a href={`http://${project.url}`}>{project.url}</a>
+				</div>
+			</div>
+		</div>
+  );
 }
 
 export const pageQuery = graphql`
@@ -53,3 +51,5 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+export default ProjectTemplate;
